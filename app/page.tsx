84b7911519610b505c2161727a0e5979b6dcba8a
zivf1990 <fisher.ziv@gmail.com>
2024-01-27 16:04:18 +0200

    fix: changed style

--- app/page.tsx
+++ app/page.tsx
@@ -14,18 +14,16 @@ export default function Home() {
       <div className="space-y-6 text-center">
         <h1
           className={cn(
-            "text-6xl font-semibold text-white drop-shadow-md",
+            "text-6xl font-semibold drop-shadow-xl",
             font.className
           )}
         >
           {process.env.APPLICATION_NAME}
         </h1>
-        <p className="text-white text-lg">A simple authentication service</p>
+        <p className="text-lg">A simple authentication service</p>
         <div>
           <LoginButton>
-            <Button variant="secondary" size="lg">
-              Sign in
-            </Button>
+            <Button size="lg">Sign in</Button>
           </LoginButton>
         </div>
       </div>
